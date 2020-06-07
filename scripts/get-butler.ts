f4b784b4a076eae464cda2226a7131505138af3e
Lachlan Kingsford <lachlan@nerdygentleman.com>
2020-06-07 22:28:00 +1000

    Download butler and push in JS

--- scripts/get-butler.ts
+++ scripts/get-butler.ts
@@ -1,26 +1,48 @@
 import fs = require('fs')
 import os = require('os')
-import http = require('http')
+import path = require('path')
+import urllib = require('urllib')
+import AdmZip = require('adm-zip')
 
-let uri: string = "" 
-let temp_location: string = "/tmp/butler.zip"
+function main() {
+    let uri: string = "";
+    let zipFileStream: fs.WriteStream;
 
-switch (os.type())
-{
-    case "Linux":
-        uri = "https://broth.itch.ovh/butler/windows-amd64/LATEST/archive/default";
-        break;
-    case "Darwin":
-        uri = "https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default";
-        break;
-    case "Windows":
-        uri = "https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default";
-        break;
+    if (!fs.existsSync("utils"))
+    {
+        fs.mkdirSync("utils");
+    }
+
+    switch (os.type()) {
+        case "Linux":
+            uri = "https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default";
+            break;
+        case "Darwin":
+            uri = "https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default";
+            break;
+        case "Windows_NT":
+            uri = "https://broth.itch.ovh/butler/windows-amd64/LATEST/archive/default";
+            break;
+        default:
+            console.warn("Unsupported OS for Butler - Publish to itch.io not available")
+            return;
+    }
+
+    urllib.request(uri, {followRedirect: true}).then((result: urllib.HttpClientResponse<Buffer>) => {
+        if (result.res.statusCode != 200)
+        {
+            console.error("Failed to download butler - Error %s", result.status)
+            return;
+        }
+        let z = new AdmZip(result.data)
+        z.extractAllTo('utils', true);
+    } );
 }
 
+    /* $uri =
+    new-item -itemtype Directory -Force tmp
+    invoke-webrequest -uri $uri -OutFile tmp/butler.zip
+    expand-archive -path tmp\butler.zip -DestinationPath .\utils -Force
+    remove-item tmp -Force -Recurse */
 
-/* $uri = 
-new-item -itemtype Directory -Force tmp
-invoke-webrequest -uri $uri -OutFile tmp/butler.zip
-expand-archive -path tmp\butler.zip -DestinationPath .\utils -Force
-remove-item tmp -Force -Recurse */
+main()
--- scripts/push-butler.ts
+++ scripts/push-butler.ts
@@ -1,1 +1,28 @@
+import os = require('os');
+import path = require('path');
+const exec = require('child_process').exec;
+
 //./utils/butler.exe push dist_prod nerdygentleman/itch-test:game
+
+let channel: string = process.argv[2]
+
+if (os.type() != "Darwin" && os.type() != "Windows_NT" && os.type() != "Linux") {
+    throw new Error("Not supported OS")
+}
+
+let butlerPath = path.join("utils", "butler")
+if (os.type() === "Windows_NT") {
+    butlerPath += ".exe"
+}
+
+exec(butlerPath + " push dist_prod " + channel, (err: any, stdout: any, stderr: any) => {
+    if (err) {
+        console.log(`error: ${err.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+})
