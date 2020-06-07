25dfe27a41a760346571ca62ffbadcdfc485f932
Lachlan Kingsford <lachlan@nerdygentleman.com>
2020-06-07 17:06:55 +1000

    Publish to itch from npm on mac, linux + windows

---
+++ scripts/get-butler.ts
@@ -0,0 +1,53 @@
+import fs = require('fs')
+import os = require('os')
+import path = require('path')
+import urllib = require('urllib')
+import AdmZip = require('adm-zip')
+
+function main() {
+    let uri: string = "";
+    let zipFileStream: fs.WriteStream;
+
+    if (!fs.existsSync("utils"))
+    {
+        fs.mkdirSync("utils");
+    }
+
+    switch (os.type()) {
+        case "Linux":
+            uri = "https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default";
+            break;
+        case "Darwin":
+            uri = "https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default";
+            break;
+        case "Windows_NT":
+            uri = "https://broth.itch.ovh/butler/windows-amd64/LATEST/archive/default";
+            break;
+        default:
+            console.warn("Unsupported OS for Butler - Publish to itch.io not available")
+            return;
+    }
+
+    urllib.request(uri, {followRedirect: true}).then((result: urllib.HttpClientResponse<Buffer>) => {
+        if (result.res.statusCode != 200)
+        {
+            console.error("Failed to download butler - Error %s", result.status)
+            return;
+        }
+        let z = new AdmZip(result.data)
+        z.extractAllTo('utils', true);
+
+        if (os.type() != "Windows_NT")
+        {
+            fs.chmodSync("utils/butler", 0o777)
+        }
+    } );
+}
+
+    /* $uri =
+    new-item -itemtype Directory -Force tmp
+    invoke-webrequest -uri $uri -OutFile tmp/butler.zip
+    expand-archive -path tmp\butler.zip -DestinationPath .\utils -Force
+    remove-item tmp -Force -Recurse */
+
+main()
---
+++ scripts/push-butler.ts
@@ -0,0 +1,34 @@
+import os = require('os');
+import path = require('path');
+const exec = require('child_process').exec;
+
+//./utils/butler.exe push dist_prod nerdygentleman/itch-test:game
+
+let channel: string = process.argv[2]
+
+if (os.type() != "Darwin" && os.type() != "Windows_NT" && os.type() != "Linux") {
+    throw new Error("Not supported OS")
+}
+
+let butlerPath = path.join("utils", "butler")
+if (os.type() === "Windows_NT") {
+    butlerPath += ".exe"
+}
+else
+{
+    butlerPath = "./" + butlerPath
+}
+
+exec(butlerPath + " push dist_prod " + channel, {shell: true}, (err: any, stdout: any, stderr: any) => {
+    if (err) {
+        console.log(`error: ${err.message}`);
+        console.log(`stderr: ${stderr}`);
+        console.log(`stdout: ${stdout}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+})
