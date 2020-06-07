7b2ed323899ff29c2295a1ac4ca4f672b754a1ea
Lachlan Kingsford <lachlan@nerdygentleman.com>
2020-06-07 11:43:53 +1000

    Pixi loading into page and building

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
-import express from 'express';
+import * as PIXI from 'pixi.js'
 
-const app: any = express();
-const port: any = 3000;
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('The sedulous hyena ate the antelope!');
-});
-app.listen(port, (err: any) => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on ${port}`);
-});
+// The application will create a renderer using WebGL, if possible,
+// with a fallback to a canvas render. It will also setup the ticker
+// and the root stage PIXI.Container
+const app = new PIXI.Application();
+
+const sprites: { [key: string] : PIXI.Sprite } = {};
+
+document.body.appendChild(app.view);
+
+const loader: PIXI.Loader = PIXI.Loader.shared;
+loader.add("six", "assets/6.png");
+
+function start_game() : void {
+  app.stage.addChild(sprites['six'])
+}
+
+loader.load((loader: PIXI.Loader, resources: Partial<Record<string, PIXI.LoaderResource>>) => {
+  console.log("Resources loaded")
+  sprites['six'] = new PIXI.Sprite(resources.six?.texture);
+  console.log("Sprites created")
+  start_game()
+})
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+      contentBase: 'dist',
+      port: 3000
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+        patterns: [
+            { from: 'build/assets', to: 'assets' },
+        ]
+    }),
+    new HTMLWebpackPlugin()
+  ]
+};
