dd7c87ec10299de4a2348b1f9960a63e77c665f0
Lachlan Kingsford <lachlan@nerdygentleman.com>
2020-06-10 21:23:06 +1000

    Show cards progress

--- src/game.ts
+++ src/game.ts
@@ -51,6 +51,14 @@ const CARD_WIDTH = 60;
 const CARD_HEIGHT = 48;
 const HAND_WIDTH = 256;
 const HAND_HEIGHT = 256;
+const TABLE_WIDTH = 8;
+const TABLE_HEIGHT = 8;
+
+interface cell {
+    backSprite: PIXI.Sprite;
+    suitSprite: PIXI.Sprite;
+    rankSprite: PIXI.Sprite;
+}
 
 export class Game extends State {
     constructor (app: PIXI.Application, resources: { [index: string]: PIXI.LoaderResource }) {
@@ -68,8 +76,34 @@ export class Game extends State {
     }
 
     startNewGame(): void {
-
+        this.cells = [];
+        for (let x = 0; x < TABLE_WIDTH; ++x)
+        {
+            this.cells[x] = []
+            for (let y = 0; y < TABLE_HEIGHT; ++y)
+            {
+                this.cells[x][y] = {
+                    backSprite: new PIXI.Sprite(Game.back_textures[Back.Available]),
+                    suitSprite: new PIXI.Sprite(Game.suit_textures[Suit.Diamond]),
+                    rankSprite: new PIXI.Sprite(Game.rank_textures[Rank._10])
+                }
+                let backSprite = this.cells[x][y].backSprite;
+                let suitSprite = this.cells[x][y].suitSprite;
+                let rankSprite = this.cells[x][y].rankSprite;
+                backSprite.x = x * CARD_WIDTH;
+                backSprite.y = y * CARD_HEIGHT;
+                suitSprite.x = (x + 0.5) * CARD_WIDTH;
+                suitSprite.y = y * CARD_HEIGHT;
+                rankSprite.x = x * CARD_WIDTH;
+                rankSprite.y = y * CARD_HEIGHT;
+                this.container.addChild(backSprite);
+                this.container.addChild(rankSprite);
+                this.container.addChild(suitSprite);
+            }
+        }
     }
+
+    cells: cell[][] = [];
 
     static addResources(loader: PIXI.Loader) {
         loader.add("cards_texture", "assets/Cards.png");
@@ -82,28 +116,31 @@ export class Game extends State {
     static hand_textures: {[index: number]: PIXI.Texture};
     static getTextures(resources: { [index: string] : PIXI.LoaderResource })
     {
+        Game.suit_textures = {};
         for (let i = 0; i < 4; i++) {
             let width = CARD_WIDTH / 2;
             let height = CARD_HEIGHT;
             let srcX = i * width;
             let srcY = 0;
-            this.suit_textures[i] = new PIXI.Texture(resources["cards_texture"].data as PIXI.BaseTexture, new PIXI.Rectangle(srcX, srcY, width, height));
+            Game.suit_textures[i] = new PIXI.Texture(resources["cards_texture"].texture.baseTexture as PIXI.BaseTexture, new PIXI.Rectangle(srcX, srcY, width, height));
         }
 
+        Game.rank_textures = {};
         for (let i = 0; i < 26; i++) {
             let width = CARD_WIDTH / 2;
             let height = CARD_HEIGHT;
             let srcX = (i % 13) * width;
             let srcY = (1 + Math.floor(i / 13)) * height;
-            this.rank_textures[i] = new PIXI.Texture(resources["cards_texture"].data as PIXI.BaseTexture, new PIXI.Rectangle(srcX, srcY, width, height));
+            Game.rank_textures[i] = new PIXI.Texture(resources["cards_texture"].texture.baseTexture as PIXI.BaseTexture, new PIXI.Rectangle(srcX, srcY, width, height));
         }
 
+        Game.back_textures = {};
         for (let i = 0; i < 5; i++) {
             let width = CARD_WIDTH;
             let height = CARD_HEIGHT;
             let srcX = (2 + i) * width;
             let srcY = 0;
-            this.back_textures[i] = new PIXI.Texture(resources["cards_texture"].data as PIXI.BaseTexture, new PIXI.Rectangle(srcX, srcY, width, height));
+            Game.back_textures[i] = new PIXI.Texture(resources["cards_texture"].texture.baseTexture as PIXI.BaseTexture, new PIXI.Rectangle(srcX, srcY, width, height));
         }
     }
 }
